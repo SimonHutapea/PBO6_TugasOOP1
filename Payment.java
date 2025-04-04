b737b0639805ee300945e7fcbd495e61b2c92ddc
SimonHutapea <162656586+SimonHutapea@users.noreply.github.com>
2025-04-04 11:08:39 +0700

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -20,7 +20,7 @@ public class Payment {
         this.paymentMethod = paymentMethod;
         double discountAmount = (discountPercent / 100) * totalPrice;
         totalPrice -= discountAmount;
-        this.paymentStatus = "Pembayaran Berhasil dengan " + paymentMethod;
+        this.paymentStatus = "Pembayaran Berhasil";
     }
 
     // Menampilkan detail pembayaran
